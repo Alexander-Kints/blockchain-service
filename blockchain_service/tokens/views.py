d4c1b9bbc5c884254dd3d3911b52a5d85775067f
Александр Кинцлер <alexander.kintsler@yandex.ru>
2024-10-19 17:57:54 +0300

    web3 service and createTokenView

--- blockchain_service/tokens/views.py
+++ blockchain_service/tokens/views.py
@@ -1,17 +1,30 @@
+import os
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
 from .serializers import TokenSerializer
 from .models import Token
+from .web3_service import Web3Service, generate_random_str
 
 
 class TokenCreateAPIView(APIView):
     def post(self, request):
         model_serializer = TokenSerializer(data=request.data)
         model_serializer.is_valid(raise_exception=True)
-        model_serializer.save(unique_hash="testhash123", tx_hash="terterg")
-        print(model_serializer.data)
+        unique_hash = generate_random_str(20)
+        service = Web3Service(
+            network_url=os.environ.get('NFT_NETWORK_URL'),
+            contract_address=os.environ.get('NFT_CONTRACT_ADDRESS'),
+            abi_filepath='static/ERC_721_abi.json'
+        )
+        tx_hash = service.create_token(
+            media_url=request.data['media_url'],
+            unique_hash=unique_hash,
+            owner=request.data['owner'],
+            private_key=os.environ.get('NFT_PRIVATE_KEY')
+        )
+        model_serializer.save(unique_hash=unique_hash, tx_hash=tx_hash)
         return Response(model_serializer.data)
 
 
---
+++ blockchain_service/tokens/web3_service.py
@@ -0,0 +1,60 @@
+from string import ascii_letters, digits
+from random import choice
+import json
+from web3 import Web3
+from web3.exceptions import Web3Exception
+
+
+class Web3Service:
+    def __init__(self, network_url: str, contract_address: str,
+                 abi_filepath: str) -> None:
+        self.__network_url = network_url
+        self.__contract_address = contract_address
+        self.__abi_filepath = abi_filepath
+
+    def create_token(self, media_url: str, unique_hash: str,
+                     owner: str, private_key: str) -> str:
+        try:
+            web3 = Web3(Web3.HTTPProvider(self.__network_url))
+            abi = json.loads(open(self.__abi_filepath).read())
+            checksum_owner = Web3.to_checksum_address(
+                owner
+            )
+            contract = web3.eth.contract(
+                Web3.to_checksum_address(self.__contract_address), abi=abi
+            )
+
+            dict_transaction = {
+                'chainId': web3.eth.chain_id,
+                'from': checksum_owner,
+                'gasPrice': web3.eth.gas_price,
+                'nonce': web3.eth.get_transaction_count(
+                    checksum_owner),
+            }
+            transaction = contract.functions.mint(
+                checksum_owner, unique_hash, media_url
+            ).build_transaction(dict_transaction)
+            signed_tx = web3.eth.account.sign_transaction(
+                transaction, private_key)
+            tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
+            return tx_hash.hex()
+        except Web3Exception as e:
+            print(e)
+            return ""
+
+    def total_supply(self) -> int:
+        try:
+            web3 = Web3(Web3.HTTPProvider(self.__network_url))
+            abi = json.loads(open(self.__abi_filepath).read())
+            contract = web3.eth.contract(
+                Web3.to_checksum_address(self.__contract_address), abi=abi
+            )
+            return contract.functions.totalSupply().call()
+        except Web3Exception as e:
+            print(e)
+            return 0
+
+
+def generate_random_str(length: int) -> str:
+    source = ascii_letters + digits
+    return ''.join(choice(source) for _ in range(length))
